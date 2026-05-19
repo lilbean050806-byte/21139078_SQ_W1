0084733f8c18f45af46b5d74a2a5c60195c958a3
lilbean050806-byte <lilbean050806@gmail.com>
2026-05-18 22:17:03 -0400

    updated code

--- W1_EX01/sketch.js
+++ W1_EX01/sketch.js
@@ -1,88 +1,24 @@
-let currentColor;
-let brushSize = 10;
-
-let slider;
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-
-  background(255);
-
-  currentColor = color(0);
-
-  createUI();
+  createCanvas(400, 400);
+  rectMode(CENTER);
+  noStroke();
 }
 
 function draw() {
+  background(220);
 
-  // Drawing
-  if (mouseIsPressed && mouseY > 60) {
-    stroke(currentColor);
-    strokeWeight(brushSize);
-    line(pmouseX, pmouseY, mouseX, mouseY);
-  }
-}
+  let x = width / 2;
+  let y = height / 2;
 
-function createUI() {
 
-  // Red button
-  let redBtn = createButton("Red");
-  redBtn.position(10, 10);
-  redBtn.mousePressed(() => {
-    currentColor = color(255, 0, 0);
-  });
+  fill(194, 140, 80);
 
-  // Blue button
-  let blueBtn = createButton("Blue");
-  blueBtn.position(60, 10);
-  blueBtn.mousePressed(() => {
-    currentColor = color(0, 100, 255);
-  });
 
-  // Green button
-  let greenBtn = createButton("Green");
-  greenBtn.position(120, 10);
-  greenBtn.mousePressed(() => {
-    currentColor = color(0, 255, 100);
-  });
+  rect(x - 35, y + 40, 70, 20, 3);
+  rect(x + 35, y + 40, 70, 20, 3);
 
-  // Black button
-  let blackBtn = createButton("Black");
-  blackBtn.position(190, 10);
-  blackBtn.mousePressed(() => {
-    currentColor = color(0);
-  });
+  rect(x, y, 20, 70, 3);
 
-  // Eraser
-  let eraserBtn = createButton("Eraser");
-  eraserBtn.position(260, 10);
-  eraserBtn.mousePressed(() => {
-    currentColor = color(255);
-  });
-
-  // Clear canvas
-  let clearBtn = createButton("Clear");
-  clearBtn.position(330, 10);
-  clearBtn.mousePressed(() => {
-    background(255);
-  });
-
-  // Save button
-  let saveBtn = createButton("Save");
-  saveBtn.position(390, 10);
-  saveBtn.mousePressed(() => {
-    saveCanvas("myDrawing", "png");
-  });
-
-  // Brush size slider
-  slider = createSlider(1, 50, 10);
-  slider.position(470, 15);
-}
-
-function mouseDragged() {
-  brushSize = slider.value();
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+  rect(x - 35, y - 40, 70, 20, 3);
+  rect(x + 35, y - 40, 70, 20, 3);
 }
